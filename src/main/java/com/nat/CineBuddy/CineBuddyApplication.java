d2247e15fbc6acb2a9800c5382935f765a6389f1
smfulton <sfulton1996@gmail.com>
2024-12-05 20:19:29 -0600

    Real first commit

---
+++ src/main/java/com/nat/CineBuddy/CineBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.nat.CineBuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nat/CineBuddy/CineBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nat.CineBuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
