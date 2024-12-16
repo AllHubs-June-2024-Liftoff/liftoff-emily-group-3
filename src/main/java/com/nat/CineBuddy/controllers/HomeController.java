29378747ae371dfd8374e408890620c92ba60c08
riverkahrhoff <river.kahrhoff@gmail.com>
2024-12-16 15:17:04 -0600

    api functional, homepage html and css still needs work

---
+++ src/main/java/com/nat/CineBuddy/controllers/HomeController.java
@@ -0,0 +1,28 @@
+package com.nat.CineBuddy.controllers;
+
+import com.nat.CineBuddy.models.Movie;
+import com.nat.CineBuddy.service.TMDbService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+    private final TMDbService tmDbService;
+
+    public HomeController(TMDbService tmDbService) {
+        this.tmDbService = tmDbService;
+    }
+
+
+    @GetMapping("/")
+    public String trendingMovies(Model model) {
+        List<Movie> movies = tmDbService.getTrendingMovies();
+        model.addAttribute("movies", movies);
+        return "index";
+    }
+
+}
+
---
+++ src/main/java/com/nat/CineBuddy/models/Movie.java
@@ -0,0 +1,127 @@
+package com.nat.CineBuddy.models;
+
+
+public class Movie {
+    private String id;
+    private String title;
+    private String overview;
+    private String releaseDate;
+    private String posterPath;
+    private String genres;
+    private String budget;
+    private String revenue;
+    private String runtime;
+    private String voteAverage;
+
+    // Constructors
+
+    //empty constructor for sql database connection
+    public Movie() {
+    }
+
+
+    public Movie(String id, String title, String overview, String releaseDate, String posterPath) {
+        this.id = id;
+        this.title = title;
+        this.overview = overview;
+        this.releaseDate = releaseDate;
+        this.posterPath = posterPath;
+    }
+
+    public Movie(String id, String title, String overview, String releaseDate, String posterPath, String genres, String budget, String revenue, String runtime, String voteAverage) {
+        this.id = id;
+        this.title = title;
+        this.overview = overview;
+        this.releaseDate = releaseDate;
+        this.posterPath = posterPath;
+        this.genres = genres;
+        this.budget = budget;
+        this.revenue = revenue;
+        this.runtime = runtime;
+        this.voteAverage = voteAverage;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getGenres() {
+        return genres;
+    }
+
+    public void setGenres(String genres) {
+        this.genres = genres;
+    }
+
+    public String getBudget() {
+        return budget;
+    }
+
+    public void setBudget(String budget) {
+        this.budget = budget;
+    }
+
+    public String getRevenue() {
+        return revenue;
+    }
+
+    public void setRevenue(String revenue) {
+        this.revenue = revenue;
+    }
+
+    public String getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(String runtime) {
+        this.runtime = runtime;
+    }
+
+    public String getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(String voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    public String getFormattedGenres() {
+        return String.join(", ", this.genres);
+    }
+}
