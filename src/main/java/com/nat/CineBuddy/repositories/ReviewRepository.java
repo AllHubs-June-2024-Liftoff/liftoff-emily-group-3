0c159ca9588c91b7f9072ac544bb38b8ea02f429
riverkahrhoff <river.kahrhoff@gmail.com>
2024-12-19 19:02:40 -0600

    now able to delete reviews from user profile

--- src/main/java/com/nat/CineBuddy/repositories/ReviewRepository.java
+++ src/main/java/com/nat/CineBuddy/repositories/ReviewRepository.java
@@ -1,6 +1,7 @@
 package com.nat.CineBuddy.repositories;
 
 import com.nat.CineBuddy.models.Review;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -8,4 +9,5 @@ import java.util.List;
 public interface ReviewRepository extends CrudRepository<Review, Long> {
     List<Review> findByMovieId(String movieId);
     List<Review> findByUsername(String username);
+
 }
